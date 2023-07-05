0fc82740712ef501dfab208495bc16f4e1c1e2e1
JuampiKeim <juampikeim@gmail.com>
2023-07-05 14:12:09 -0300

    Primer commit proyecto

---
+++ src/components/Home.vue
@@ -0,0 +1,95 @@
+<template>
+    <Layout>
+        <template #header>
+            <Header></Header>
+        </template>
+        <template #resume>
+            <Resume
+                :label="'Ahorro total'"
+                :total-amount="100000"
+                :amount="amount"
+            >
+                <template #graphic>
+                    graphic
+                </template>
+                <template #action>
+                    action
+                </template>
+            </Resume>
+        </template>
+        <template #movements>
+            <Movements
+                :movements="movements"
+            />
+        </template>
+    </Layout>
+</template>
+
+<script>
+import Layout from './Layout.vue';
+import Header from './Header.vue';
+import Resume from './Resume/Index.vue';
+import Movements from './Movements/Index.vue';
+
+export default {
+    components: {
+        Layout,
+        Header,
+        Resume,
+        Movements,
+    },
+    data() {
+        return {
+            amount: null,
+            label: null,
+            movements: [{
+                id: 0,
+                title: "Movimiento 1",
+                description: "Aprende algo dinero.",
+                amount: 1000,
+            },{
+                id: 1,
+                title: "Movimiento 2",
+                description: "Aprende algo dinero.",
+                amount: 1000,
+            },{
+                id: 2,
+                title: "Movimiento 3",
+                description: "Aprende algo dinero.",
+                amount: 1000,
+            },{
+                id: 3,
+                title: "Movimiento 4",
+                description: "Aprende algo dinero.",
+                amount: 1000,
+            },{
+                id: 4,
+                title: "Movimiento 5",
+                description: "Aprende algo dinero.",
+                amount: 1000,
+            },{
+                id: 5,
+                title: "Movimiento 6",
+                description: "Aprende algo dinero.",
+                amount: 1000,
+            },{
+                id: 6,
+                title: "Movimiento 7",
+                description: "Aprende algo dinero.",
+                amount: 1000,
+            },{
+                id: 7,
+                title: "Movimiento 8",
+                description: "Aprende algo dinero.",
+                amount: 1000,
+            },{
+                id: 8,
+                title: "Movimiento 9",
+                description: "Aprende algo dinero.",
+                amount: 1000,
+            }
+        ]
+        }
+    },
+}
+</script>
---
+++ src/components/SplashScreen.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="splashscreen">
+        <img 
+            src="@/assets/dinerodinero-icon.svg" 
+            alt="Logo" 
+        />
+        <h1><span>dinero</span>dinero</h1>
+    </div>
+</template>
+
+<style scoped>
+.splashscreen {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  height: 100vh;
+}
+h1 {
+  color: var(--brand-blue);
+}
+h1 span {
+  color: var(--brand-green);
+}
+</style>
